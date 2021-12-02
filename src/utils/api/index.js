9f88403bb23c83198e35eaec2eae3906410547fd
Mike Walchak <mfwalchak@gmail.com>
2021-12-02 15:09:09 -0700

    ready for heroku deployment

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -3,7 +3,7 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
